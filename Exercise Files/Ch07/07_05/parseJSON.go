a6b37fc2fccaf7665dca02889e17c8837d3ea947
lucasoliveira <lucas.gomes.oliveira@hotmail.com>
2020-06-13 20:56:03 -0300

    dia 13

--- Exercise Files/Ch07/07_05/parseJSON.go
+++ Exercise Files/Ch07/07_05/parseJSON.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	// "encoding/json"
-	// "strings"
-	// "math/big"
+	"net/http"
+	"strings"
 )
+
+type Tour struct {
+	Name, Price string
+}
 
 func main() {
 
 	url := "http://services.explorecalifornia.org/json/tours.php"
 	content := contentFromServer(url)
 
-	fmt.Println(content)
+	tours := toursOfJson(content)
+
+	fmt.Println(tours)
 
 }
 
@@ -25,10 +30,10 @@ func checkError(err error) {
 }
 
 func contentFromServer(url string) string {
-	
+
 	resp, err := http.Get(url)
 	checkError(err)
-	
+
 	defer resp.Body.Close()
 	bytes, err := ioutil.ReadAll(resp.Body)
 	checkError(err)
@@ -36,3 +41,21 @@ func contentFromServer(url string) string {
 	return string(bytes)
 }
 
+func toursOfJson(content string) []Tour {
+
+	tours := make([]Tour, 0, 20)
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+
+	return tours
+
+}
